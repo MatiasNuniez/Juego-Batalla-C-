9767d9837c86ab3f9965f3e149efdf53db0d738a
Matias Nunez <matiasnuniez192@gmail.com>
2024-12-19 14:54:39 -0300

    Add project files.

---
+++ Juego-Batalla/Program.cs
@@ -0,0 +1,40 @@
+﻿class Criatura
+{
+    public string Nombre { get; set; }
+    public int Puntos { get; set; }
+    public int PoderAtaque {  get; set; }
+    public Criatura(string nombre, int puntos, int poderataque)
+    {
+        this.Nombre = nombre;
+        this.Puntos = puntos;
+        this.PoderAtaque = poderataque;
+    }
+    public virtual void Atacar()
+    {
+        if (Puntos > PoderAtaque)
+        {
+            Puntos -= PoderAtaque;
+        }
+    }
+
+    public virtual void RecibirAtaque()
+    {
+
+    }
+
+}
+
+
+class Guerrero: Criatura 
+{
+    public Guerrero(string nombre, int puntos, int poderAtaque) : base(nombre, puntos, poderAtaque) { }
+
+
+}
+
+class Mago : Criatura
+{
+    public Mago(string nombre, int puntos, int poderAtaque) : base(nombre, puntos, poderAtaque) { }
+
+
+}
